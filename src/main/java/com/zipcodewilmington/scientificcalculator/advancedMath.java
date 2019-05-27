917fdc7d648ec7970516b1796e6d2a360298603b
Ben Rybinski <benrybinski@zipcoders-MacBook-Pro.local>
2019-05-27 11:49:45 -0400

    added advmath methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/advancedMath.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/advancedMath.java
@@ -1,4 +1,35 @@
-package com.zipcodewilmington.scientificcalculator;
+ppackage com.zipcodewilmington.scientificcalculator;
 
 public class advancedMath {
+
+    advancedMath() {
+
+    }
+
+    public static Double sqrt(Double a) { return Math.sqrt(a);
+    }
+
+    public Double pow(Double a, Double b) { return Math.pow(a,b);
+    }
+
+    public Double sin(Double a) { return Math.sin(a);
+    }
+
+    public Double cos(Double a) { return Math.cos(a);
+    }
+
+    public Double tan(Double a) { return Math.tan(a);
+
+    }
+
+    public Double asin(Double a) { return Math.asin(a);
+    }
+
+    public Double acos(Double a) { return Math.acos(a);
+    }
+
+    public Double atan(Double a) { return Math.atan(a);
+    }
+
+
 }
